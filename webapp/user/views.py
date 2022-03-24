f640c450056dd588b08e8a25a408ba9457d0a6a4
denis-meshcheryakov <ktulhoo.pyneng@yandex.ru>
2022-03-24 23:54:35 +0300

    add menu.html

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -9,7 +9,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('news.index'))
     title = 'Авторизация'
     login_form = LoginForm()
     return render_template('user/login.html', page_title=title, form=login_form)
@@ -24,7 +24,7 @@ def procces_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('index'))
+            return redirect(url_for('news.index'))
 
     flash('Неправильное имя или пароль')
     return redirect(url_for('user.login'))
@@ -34,4 +34,4 @@ def procces_login():
 def logout():
     logout_user()
     flash('Вы успешно разлогинились')
-    return redirect(url_for('index'))
+    return redirect(url_for('news.index'))
